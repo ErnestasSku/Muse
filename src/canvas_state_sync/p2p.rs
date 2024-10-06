cd615055b30ae31c3c46495ea4a18fe9b72615d0
ErnestasSku <ernestassku@gmail.com>
2024-10-06 20:48:57 +0300

    Add message chunking

--- src/canvas_state_sync/p2p.rs
+++ src/canvas_state_sync/p2p.rs
@@ -1,16 +1,23 @@
+use bincode::{self, deserialize};
 use futures::stream::StreamExt;
+use libp2p::gossipsub::IdentTopic;
+use libp2p::Swarm;
 use libp2p::{gossipsub, mdns, noise, swarm::NetworkBehaviour, swarm::SwarmEvent, tcp, yamux};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 use tokio::sync::mpsc;
 use tokio::{io, select};
-use bincode;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::canvas_state_sync::sync_types::MessageType;
 
-const MAX_DATA_TRANSFER_SIZE: usize = 1024*1024;
+use super::sync_types::{ChunkCollector, ChunkedMessage};
+
+const MAX_DATA_TRANSFER_SIZE: usize = 1024 * 1024;
+const MAX_CHUNK_SIZE: usize = MAX_DATA_TRANSFER_SIZE
+    - std::mem::size_of::<ChunkedMessage>()
+    - std::mem::size_of::<gossipsub::Message>();
 
 #[derive(NetworkBehaviour)]
 pub struct TestBehavior {
@@ -21,7 +28,7 @@ pub struct TestBehavior {
 pub async fn p2p(
     mut gui_receiver: mpsc::Receiver<MessageType>,
     p2p_sender: mpsc::Sender<MessageType>,
-    running: Arc<AtomicBool>
+    running: Arc<AtomicBool>,
 ) {
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
@@ -74,7 +81,6 @@ pub async fn p2p(
     let topic = gossipsub::IdentTopic::new("test-net");
     swarm.behaviour_mut().gossipsub.subscribe(&topic).unwrap();
 
-    // let mut stdin = io::BufReader::new(io::stdin()).lines();
     swarm
         .listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse().unwrap())
         .unwrap();
@@ -82,50 +88,131 @@ pub async fn p2p(
         .listen_on("/ip4/0.0.0.0/tcp/0".parse().unwrap())
         .unwrap();
 
+    let mut chunk_collector = ChunkCollector::new();
     loop {
         if !running.load(Ordering::Relaxed) {
             break;
         }
-        
+
         select! {
-            event = swarm.select_next_some() => match event {
-                SwarmEvent::Behaviour(TestBehaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        println!("mDNS discovered a new peer: {peer_id}");
-                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
-                    }
-                },
-                SwarmEvent::Behaviour(TestBehaviorEvent::Mdns(mdns::Event::Expired(list))) => {
-                    for (peer_id, _multiaddr) in list {
-                        println!("mDNS discover peer has expired: {peer_id}");
-                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
-                    }
-                },
-                SwarmEvent::Behaviour(TestBehaviorEvent::Gossipsub(gossipsub::Event::Message {
-                    propagation_source: _peer_id,
-                    message_id: _id,
-                    message,
-                })) => {
-                    let deserialized: Result<MessageType, _> = bincode::deserialize(&message.data);
-
-                    if let Ok(msg) = deserialized {
-                        // TODO: figure out how would error handling even work in this case
-                        let _result = p2p_sender.send(msg).await;
-                    }
-                }
-                SwarmEvent::NewListenAddr { address, .. } => {
-                    println!("Local node is listening on {address}");
-                }
-                _ => {}
-            },
+            event = swarm.select_next_some() => {
+                handle_swarm_event(&mut swarm, &p2p_sender, event, &mut chunk_collector).await;
+            }
             Some(message) = gui_receiver.recv() => {
-                let serialized_message = bincode::serialize(&message).expect("failed to serialise");
-
-                if let Err(e) = swarm
-                    .behaviour_mut().gossipsub
-                    .publish(topic.clone(), serialized_message) {
-                }
+                handle_sending(&mut swarm, &topic, &message);
             }
         }
     }
+}
+
+async fn handle_swarm_event(
+    swarm: &mut Swarm<TestBehavior>,
+    p2p_sender: &mpsc::Sender<MessageType>,
+    event: SwarmEvent<TestBehaviorEvent>,
+    chunk_collector: &mut ChunkCollector,
+) {
+    match event {
+        SwarmEvent::Behaviour(TestBehaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
+            for (peer_id, _multiaddr) in list {
+                println!("mDNS discovered a new peer: {peer_id}");
+                swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+            }
+        }
+        SwarmEvent::Behaviour(TestBehaviorEvent::Mdns(mdns::Event::Expired(list))) => {
+            for (peer_id, _multiaddr) in list {
+                println!("mDNS discover peer has expired: {peer_id}");
+                swarm
+                    .behaviour_mut()
+                    .gossipsub
+                    .remove_explicit_peer(&peer_id);
+            }
+        }
+        SwarmEvent::Behaviour(TestBehaviorEvent::Gossipsub(gossipsub::Event::Message {
+            propagation_source: _peer_id,
+            message_id: _id,
+            message,
+        })) => {
+            // let deserialized: Result<MessageType, _> = bincode::deserialize(&message.data);
+            //
+            // if let Ok(msg) = deserialized {
+            // TODO: figure out how would error handling even work in this case
+            // let _result = p2p_sender.send(msg).await;
+            // }
+
+            let deserialized_chunk: Result<ChunkedMessage, _> = bincode::deserialize(&message.data);
+
+            if let Ok(ChunkedMessage {
+                id,
+                chunk_index,
+                total_chunks,
+                data,
+            }) = deserialized_chunk
+            {
+                chunk_collector.add_chunk(id, chunk_index, total_chunks, data);
+
+                // If the message is complete, reassemble it
+                // TODO: this can cause issues possibly. After reassemlbing the message
+                // It needs to be removed from memory.
+                // What happens if message is never reassembled?
+                if chunk_collector.is_complete(id) {
+                    if let Some(full_message) = chunk_collector.reassemble(id) {
+                        let deserialized_message: Result<MessageType, _> =
+                            bincode::deserialize(&full_message);
+                        if let Ok(msg) = deserialized_message {
+                            println!("Full message reassembled and deserialized");
+                            // Handle the full message, e.g., send to GUI
+                            let result = p2p_sender.send(msg).await;
+                            println!("Message sent back to GUI");
+                        }
+                    }
+                }
+            }
+        }
+        SwarmEvent::NewListenAddr { address, .. } => {
+            println!("Local node is listening on {address}");
+        }
+        _ => {}
+    }
+}
+
+fn handle_sending(swarm: &mut Swarm<TestBehavior>, topic: &IdentTopic, message: &MessageType) {
+    let serialized_message = bincode::serialize(message).expect("failed to serialise");
+
+    let message_id = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let total_chunks = (serialized_message.len() as f64 / MAX_CHUNK_SIZE as f64).ceil() as u32;
+
+    for (index, chunk) in serialized_message.chunks(MAX_CHUNK_SIZE).enumerate() {
+        let chunk_message = ChunkedMessage {
+            id: message_id,
+            chunk_index: index as u32,
+            total_chunks,
+            data: chunk.to_vec(),
+        };
+
+        let serialized_chunk =
+            bincode::serialize(&chunk_message).expect("Failed to serialize chunk");
+
+        // println!("{}", std::mem::size_of_val(&serialized_chunk));
+        println!("{}", serialized_chunk.len());
+
+        if let Err(e) = swarm
+            .behaviour_mut()
+            .gossipsub
+            .publish(topic.clone(), serialized_chunk)
+        {
+            println!("Publish error: {e:?}");
+            println!("{}", MAX_DATA_TRANSFER_SIZE);
+        }
+    }
+
+    // if let Err(e) = swarm
+    //     .behaviour_mut()
+    //     .gossipsub
+    //     .publish(topic.clone(), serialized_message) {
+    //         println!("error publishing the message");
+    //     }
 }
--- src/canvas_state_sync/sync_types.rs
+++ src/canvas_state_sync/sync_types.rs
@@ -1,3 +1,5 @@
+// use eframe::egui::ahash::HashMap;
+use std::collections::HashMap;
 // use eframe::emath::TSTransform;
 use serde::{Deserialize, Serialize};
 
@@ -5,6 +7,60 @@ use crate::canvas_app::App;
 
 // use crate::canvas_image::CanvasImageData;
 
+#[derive(Serialize, Deserialize)]
+pub struct ChunkedMessage {
+    pub id: u64,
+    pub chunk_index: u32,
+    pub total_chunks: u32,
+    pub data: Vec<u8>,
+}
+
+pub struct ChunkCollector {
+    chunks: HashMap<u64, Vec<Option<Vec<u8>>>>, // Mapping message ID -> list of chunks
+    chunk_sizes: HashMap<u64, u32>,             // Mapping message ID -> total number of chunks
+}
+
+impl ChunkCollector {
+    pub fn new() -> Self {
+        ChunkCollector {
+            chunks: HashMap::new(),
+            chunk_sizes: HashMap::new(),
+        }
+    }
+
+    pub fn add_chunk(&mut self, id: u64, chunk_index: u32, total_chunks: u32, data: Vec<u8>) {
+        // If it's a new message, initialize storage
+        self.chunk_sizes.entry(id).or_insert(total_chunks);
+        let chunk_list = self
+            .chunks
+            .entry(id)
+            .or_insert_with(|| vec![None; total_chunks as usize]);
+
+        // Store the chunk
+        chunk_list[chunk_index as usize] = Some(data);
+    }
+
+    pub fn is_complete(&self, id: u64) -> bool {
+        if let Some(chunk_list) = self.chunks.get(&id) {
+            return chunk_list.iter().all(|chunk| chunk.is_some());
+        }
+        false
+    }
+
+    pub fn reassemble(&self, id: u64) -> Option<Vec<u8>> {
+        if self.is_complete(id) {
+            let chunk_list = self.chunks.get(&id).unwrap();
+            let mut message_data = Vec::new();
+            for chunk in chunk_list {
+                if let Some(data) = chunk {
+                    message_data.extend_from_slice(&data);
+                }
+            }
+            return Some(message_data);
+        }
+        None
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 pub enum MessageType {
@@ -18,11 +74,12 @@ pub struct SyncableState {
     // transform : Option<TSTransform>,
     // images: Vec<CanvasImageData>,
     pub dropped_bytes: Vec<Vec<u8>>,
-    
 }
 
 impl From<&App> for SyncableState {
     fn from(value: &App) -> Self {
-        Self { dropped_bytes: value.dropped_bytes.clone() }
+        Self {
+            dropped_bytes: value.dropped_bytes.clone(),
+        }
     }
 }
